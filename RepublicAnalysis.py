d3b750bace1a9af88ff5b334f36239d5011e7dce
tuvtran <tuvtran97@gmail.com>
2018-07-21 15:21:28 -0400

    implemented second method

--- RepublicAnalysis.py
+++ RepublicAnalysis.py
@@ -6,12 +6,16 @@ class RepublicAnalysis(AbstractGutenberg):
     def __init__(self, textFile):
         self.textFile = textFile
         self.content = self._loadFileToMemory()
+        self.wordFrequencies = {}
 
     def _loadFileToMemory(self):
         return self.textFile.read()
 
     def getTotalNumberOfWords(self) -> int:
         return len(self.content.split(" "))
+
+    def getTotalUniqueWords(self) -> int:
+        return len(set(self.content.split(" ")))
 
 
 if __name__ == "__main__":
@@ -21,5 +25,6 @@ if __name__ == "__main__":
 
     print("Analyzing The Republic by Plato")
     print(f"Total number of words is {analysis.getTotalNumberOfWords()}")
+    print(f"Total number of unique words is {analysis.getTotalUniqueWords()}")
 
     novel.close()
